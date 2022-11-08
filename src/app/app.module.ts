b2dc084f229ddd619141c8aa502f946fd6d27f61
AK <101558236+AKuchko@users.noreply.github.com>
2022-11-09 02:45:33 +0700

    add heading card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadingCard } from './components/heading-card/headingCard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadingCard
   ],
   imports: [
     BrowserModule,
